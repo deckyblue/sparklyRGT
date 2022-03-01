65c1e407f4a2dab26a38796ea5440c767386cd5b
Lauren <kerk0100@mylaurier.ca>
2022-03-01 12:50:29 -0800

    nc files

---
+++ Pystan/WAIC/.ipynb_checkpoints/waic-checkpoint.py
@@ -0,0 +1,272 @@
+###PACKAGES###
+import numpy as np
+import os 
+import arviz as az
+import pandas as pd
+import math
+import xarray as xr
+import model_data as md
+
+def md_subs(df, numsessions, subs): 
+    """gets md_df and unique subs (without subject 0)"""
+    md_dict = md.get_model_data(df, numsessions, subs) #model data as a dict
+    md_df = pd.DataFrame.from_dict(md_dict) #model data as a df
+    
+    subs_unique = md_df.startSubject.unique()
+    subs_unique = np.delete(subs_unique, 1) #remove subject 0 (not a real subject)
+    
+    return md_df, subs_unique
+
+def ntr_sub(md_df, subs_unique): 
+    """ntr_sub takes in md_df, and gets the range of trials for a given subject within subs"""
+    
+    ntr_sub_ranges = []
+    ntr_df = pd.DataFrame({'subject#': subs_unique})
+    
+    #get range for sub: 
+    for s in subs_unique: 
+        first_t = md_df.index[md_df['startSubject'] == s].tolist() #first trial
+        last_t = md_df.index[md_df['startSubject'] == (s+1)].tolist() #last trial
+        
+        if last_t != []: #if s+1 does not exist (when s = final subject#), last_t = []
+            ntr_sub = list(range(first_t[0], last_t[0]))
+            ntr_sub_ranges.append(ntr_sub)
+        elif last_t == []:
+            last_t = [len(md_df)] #last_t = length of md_df
+            ntr_sub = list(range(first_t[0], last_t[0]))
+            ntr_sub_ranges.append(ntr_sub)
+        
+    ntr_df['range_ntr'] = ntr_sub_ranges
+    return ntr_df #outputs df with subject, and range_ntr as the next column
+
+def chosen_outcome_data(md_df, ntr_df, subs_unique): 
+    """takes in md_df, subs_unique and gets chosen and outcome data"""
+    
+    for row in md_df.index: #add outcome column (where loss == 0, and win == 1)
+        if md_df.at[row,'R'] == 0: #loss
+            md_df.at[row,'outcome'] = 0
+        if md_df.at[row,'P'] == 0: #win
+            md_df.at[row,'outcome'] = 1
+    
+    chosen = []
+    outcome = []
+    ntr_df = ntr_sub(md_df, subs_unique)
+    
+    for row in ntr_df.index:
+        range_ts = ntr_df.at[row, 'range_ntr'] #range of trials
+        filtered_md_df = md_df[range_ts[0]:(range_ts[-1]+1)]
+        chosen.append(filtered_md_df['O'].values)
+        outcome.append(filtered_md_df['outcome'].values)
+                               
+    return chosen, outcome
+
+def get_params(fit, subs_unique, model):
+    """takes in fit, subs_unique, and model name (str) ("basic, basicstar, pscale, pscalestar, pindep or pindepstar"), and outputs n sets of parameters"""
+    
+    if model == "basic": 
+        fit_beta_df = fit.posterior.beta.to_dataframe()
+        fit_etaP_df = fit.posterior.etaPositive.to_dataframe()
+        fit_etaN_df = fit.posterior.etaNegative.to_dataframe()
+        fit_m_df = fit.posterior.m.to_dataframe()
+
+        params_s = []
+
+        for s in subs_unique:
+            params = np.concatenate([fit_beta_df[fit_beta_df.index.get_level_values('beta_dim_0').isin([s-1])],
+                                      fit_etaP_df[fit_etaP_df.index.get_level_values('etaPositive_dim_0').isin([s-1])],
+                                      fit_etaN_df[fit_etaN_df.index.get_level_values('etaNegative_dim_0').isin([s-1])],
+                                      fit_m_df[fit_m_df.index.get_level_values('m_dim_0').isin([s-1])]],
+                                      axis = 1)
+            params_s.append(params)
+        return params_s
+    
+    if model == "basicstar":
+        fit_beta_df = fit.posterior.beta.to_dataframe()
+        fit_eta_df = fit.posterior.eta.to_dataframe()
+        fit_m_df = fit.posterior.m.to_dataframe()
+
+        params_s = []
+
+        for s in subs_unique:
+            params = np.concatenate([fit_beta_df[fit_beta_df.index.get_level_values('beta_dim_0').isin([s-1])],
+                                      fit_eta_df[fit_eta_df.index.get_level_values('eta_dim_0').isin([s-1])],
+                                      fit_m_df[fit_m_df.index.get_level_values('m_dim_0').isin([s-1])]],
+                                      axis = 1)
+            params_s.append(params)
+        return params_s
+    
+    if model == "pscale":
+        fit_beta_df = fit.posterior.beta.to_dataframe()
+        fit_etaP_df = fit.posterior.etaPositive.to_dataframe()
+        fit_etaN_df = fit.posterior.etaNegative.to_dataframe()
+        fit_m_df = fit.posterior.m.to_dataframe()
+        fit_b_df = fit.posterior.b.to_dataframe()
+
+        params_s = []
+
+        for s in subs_unique:
+            params = np.concatenate([fit_beta_df[fit_beta_df.index.get_level_values('beta_dim_0').isin([s-1])],
+                                      fit_etaP_df[fit_etaP_df.index.get_level_values('etaPositive_dim_0').isin([s-1])],
+                                      fit_etaN_df[fit_etaN_df.index.get_level_values('etaNegative_dim_0').isin([s-1])],
+                                      fit_m_df[fit_m_df.index.get_level_values('m_dim_0').isin([s-1])],
+                                      fit_b_df[fit_b_df.index.get_level_values('b_dim_0').isin([s-1])]],
+                                      axis = 1)
+            params_s.append(params)
+        return params_s
+    
+    if model == "pscalestar":
+        fit_beta_df = fit.posterior.beta.to_dataframe()
+        fit_eta_df = fit.posterior.eta.to_dataframe()
+        fit_m_df = fit.posterior.m.to_dataframe()
+        fit_b_df = fit.posterior.b.to_dataframe()
+
+        params_s = []
+
+        for s in subs_unique:
+            params = np.concatenate([fit_beta_df[fit_beta_df.index.get_level_values('beta_dim_0').isin([s-1])],
+                                      fit_eta_df[fit_eta_df.index.get_level_values('eta_dim_0').isin([s-1])],
+                                      fit_m_df[fit_m_df.index.get_level_values('m_dim_0').isin([s-1])],
+                                      fit_b_df[fit_b_df.index.get_level_values('b_dim_0').isin([s-1])]],
+                                      axis = 1)
+            params_s.append(params)
+        return params_s
+    
+    if model == "pindep":
+        fit_beta_df = fit.posterior.beta.to_dataframe()
+        fit_etaP_df = fit.posterior.etaPositive.to_dataframe()
+        fit_etaN_df = fit.posterior.etaNegative.to_dataframe()
+        fit_p1_df = fit.posterior.p1.to_dataframe()
+        fit_p2_df = fit.posterior.p2.to_dataframe()
+        fit_p3_df = fit.posterior.p3.to_dataframe()
+        fit_p4_df = fit.posterior.p4.to_dataframe()
+
+        params_s = []
+
+        for s in subs_unique:
+            params = np.concatenate([fit_beta_df[fit_beta_df.index.get_level_values('beta_dim_0').isin([s-1])],
+                                      fit_etaP_df[fit_etaP_df.index.get_level_values('etaPositive_dim_0').isin([s-1])],
+                                      fit_etaN_df[fit_etaN_df.index.get_level_values('etaNegative_dim_0').isin([s-1])],
+                                      fit_p1_df[fit_p1_df.index.get_level_values('p1_dim_0').isin([s-1])],
+                                      fit_p2_df[fit_p2_df.index.get_level_values('p2_dim_0').isin([s-1])],
+                                      fit_p3_df[fit_p3_df.index.get_level_values('p3_dim_0').isin([s-1])],
+                                      fit_p4_df[fit_p4_df.index.get_level_values('p4_dim_0').isin([s-1])]],
+                                      axis = 1)
+            params_s.append(params)
+        return params_s
+
+    if model == "pindepstar":
+        fit_beta_df = fit.posterior.beta.to_dataframe()
+        fit_eta_df = fit.posterior.eta.to_dataframe()
+        fit_p1_df = fit.posterior.p1.to_dataframe()
+        fit_p2_df = fit.posterior.p2.to_dataframe()
+        fit_p3_df = fit.posterior.p3.to_dataframe()
+        fit_p4_df = fit.posterior.p4.to_dataframe()
+
+        params_s = []
+
+        for s in subs_unique:
+            params = np.concatenate([fit_beta_df[fit_beta_df.index.get_level_values('beta_dim_0').isin([s-1])],
+                                      fit_eta_df[fit_eta_df.index.get_level_values('eta_dim_0').isin([s-1])],
+                                      fit_p1_df[fit_p1_df.index.get_level_values('p1_dim_0').isin([s-1])],
+                                      fit_p2_df[fit_p2_df.index.get_level_values('p2_dim_0').isin([s-1])],
+                                      fit_p3_df[fit_p3_df.index.get_level_values('p3_dim_0').isin([s-1])],
+                                      fit_p4_df[fit_p4_df.index.get_level_values('p4_dim_0').isin([s-1])]],
+                                      axis = 1)
+            params_s.append(params)
+        return params_s
+    
+def get_sub_df(df, numsessions, subs, fit, model): 
+    md_df, subs_unique = md_subs(df, numsessions, subs)
+    ntr_df = ntr_sub(md_df, subs_unique)
+    chosen_data, outcome_data = chosen_outcome_data(md_df, ntr_df, subs_unique)
+    params_s = get_params(fit, subs_unique, model)
+    
+    ntr_df['chosen_data'] = chosen_data
+    ntr_df['outcome_data'] = outcome_data
+    ntr_df['params_s'] = params_s
+    
+    return ntr_df, md_df
+
+def log_lik_values(sub_df, model, num_param):
+    """takes in sub_df, and gets all the log_likelihood values in a list"""
+    
+    log_lik_values = [] #stores log_lik_values for all subjects
+    
+    #model
+    p_win = [0.9,0.8,0.5,0.4]
+    win_amount = [1,2,3,4]
+    pun_dur = [5,10,30,40]
+    
+    for s in sub_df.index: #note: s is 0-indexed
+        
+        #subject data
+        ntr = sub_df.at[s,'range_ntr']
+        chosen_data = sub_df.at[s,'chosen_data']
+        outcome_data = sub_df.at[s,'outcome_data']
+        params = sub_df.at[s, 'params_s'] #sets of parameters 
+        
+        # updating model values
+        V = np.zeros(4) # [0,0,0,0]
+        # Q = np.zeros([4,ntr]) #1 array of 4 rows and ntr columns
+        # sets of parameters, for ntr trials
+        paramsXtrial = [(p, t) for p in range(num_param) for t in range(ntr[-1]+1 - ntr[0])] 
+        for (p, t) in paramsXtrial: 
+
+            if t == 0: #if using a new set of parameters (if first trial, reset)...
+                V = np.zeros(4) #reset the V values
+
+            p_action = np.exp(params[p][0]*V)/np.sum(np.exp(params[p][0]*V)) #p_action holds the probabilities of each action (P1-P4), starting at 0.25 for each
+
+            if outcome_data[t] == 1: 
+                V[chosen_data[t]-1] += params[p][1]*(win_amount[chosen_data[t]-1] - V[chosen_data[t]-1]) #subtract 1 because chosen_data stores P1-P4 as 1-4 instead 0-3
+            else:
+                if model == "basic":
+                    V[chosen_data[t]-1] += params[p][2]*(-params[p][3]*pun_dur[chosen_data[t]-1] - V[chosen_data[t]-1])
+                elif model == "basicstar": 
+                    V[chosen_data[t]-1] += params[p][1]*(-params[p][2]*pun_dur[chosen_data[t]-1] - V[chosen_data[t]-1])
+                elif model == "pscale":
+                    V[chosen_data[t]-1] += params[p][2]*(params[p][4] - params[p][3]*pun_dur[chosen_data[t]-1] - V[chosen_data[t]-1])
+                elif model == "pscalestar":
+                    V[chosen_data[t]-1] += params[p][1]*(params[p][3] - params[p][2]*pun_dur[chosen_data[t]-1] - V[chosen_data[t]-1])
+                elif model == "pindep":
+                    V[chosen_data[t]-1] += params[p][2]*(-params[p][chosen_data[t]+2]*pun_dur[chosen_data[t]-1] - V[chosen_data[t]-1])
+                else: #model == "pindepstar":
+                    V[chosen_data[t]-1] += params[p][1]*(-params[p][chosen_data[t]+1]*pun_dur[chosen_data[t]-1] - V[chosen_data[t]-1])
+            log_lik_values.append(math.log(p_action[chosen_data[t]-1])) 
+    
+    return log_lik_values
+
+def get_inference_data_log_lik(md_df, log_lik_values, fit, num_draws):
+    """takes in log_lik_values from get_log_lik_values, and creates an inference data object"""
+    
+    total_ntr = md_df.at[0, 'ntr'] #total trials in md_df, which represents the total number of trials for a cue variant 
+#     print(total_ntr)
+#     print(len(log_lik_values))
+    
+    #build index
+    iterables = [[0,1,2,3], list(range(0,num_draws)), list(range(0, total_ntr))] #4 chains, n draws, total_ntr trials
+    index = pd.MultiIndex.from_product(iterables, names=["chain", "draw", "trial"])
+    
+    #build InferenceData (id) object 
+    s_log_likelihood = pd.Series(log_lik_values, index=index)
+    df_log_likelihood = s_log_likelihood.to_frame(name = "log_likelihood")
+    x_log_likelihood = df_log_likelihood.to_xarray()
+    id_log_likelihood = fit.assign(x_log_likelihood)
+    
+    return id_log_likelihood
+
+def waic_fit(df, numsessions, subs, fit, model, draws):
+    """summary function: takes in all parameters and outputs WAIC table using az.waic() // parameters:
+    df - product of rgt.load_multiple_data,
+    numsessions = 5,
+    subs = cue variant (list), 
+    fit = .nc object storing the model and fit
+    model = model name (str)"""
+    
+    subs_df, md_df = get_sub_df(df, numsessions, subs, fit, model) #calculating subs twice
+    num_params = draws * 4
+    log_lik_values_list = log_lik_values(subs_df, model, num_params)
+    id_log_likelihood = get_inference_data_log_lik(md_df, log_lik_values_list, fit, draws)
+    
+    waic = az.waic(id_log_likelihood)
+    return waic
---
+++ Pystan/WAIC/.ipynb_checkpoints/waic_values-checkpoint.py
@@ -0,0 +1,176 @@
+###PACKAGES###
+import numpy as np
+import os 
+import arviz as az
+import pandas as pd
+import math
+import xarray as xr
+import model_data as md
+import waic as wc
+
+###ASSIGN FIT OBJECTS###
+
+loss_basic_fit = az.from_netcdf('loss_basic_fit.nc')
+outcome_basic_fit = az.from_netcdf('outcome_basic_fit.nc')
+random_basic_fit = az.from_netcdf('random_basic_fit.nc')
+reverse_basic_fit = az.from_netcdf('reverse_basic_fit.nc')
+standard_basic_fit = az.from_netcdf('standard_basic_fit.nc')
+uncued_basic_fit = az.from_netcdf('uncued_basic_fit.nc')
+
+loss_basicstar_fit = az.from_netcdf('loss_basic-star_fit.nc')
+outcome_basicstar_fit = az.from_netcdf('outcome_basic-star_fit.nc')
+random_basicstar_fit = az.from_netcdf('random_basic-star_fit.nc')
+reverse_basicstar_fit = az.from_netcdf('reverse_basic-star_fit.nc')
+standard_basicstar_fit = az.from_netcdf('standard_basic-star_fit.nc')
+uncued_basicstar_fit = az.from_netcdf('uncued_basic-star_fit.nc')
+
+loss_pscale_fit = az.from_netcdf('loss_pscale_fit.nc')
+outcome_pscale_fit = az.from_netcdf('outcome_pscale_fit.nc')
+random_pscale_fit = az.from_netcdf('random_pscale_fit.nc')
+reverse_pscale_fit = az.from_netcdf('reverse_pscale_fit.nc')
+standard_pscale_fit = az.from_netcdf('standard_pscale_fit.nc')
+uncued_pscale_fit = az.from_netcdf('uncued_pscale_fit.nc')
+
+loss_pscalestar_fit = az.from_netcdf('loss_pscale-star_fit.nc')
+outcome_pscalestar_fit = az.from_netcdf('outcome_pscale-star_fit.nc')
+random_pscalestar_fit = az.from_netcdf('random_pscale-star_fit.nc')
+reverse_pscalestar_fit = az.from_netcdf('reverse_pscale-star_fit.nc')
+standard_pscalestar_fit = az.from_netcdf('standard_pscale-star_fit.nc')
+uncued_pscalestar_fit = az.from_netcdf('uncued_pscale-star_fit.nc')
+
+loss_pindep_fit = az.from_netcdf('loss_pindep_fit.nc')
+outcome_pindep_fit = az.from_netcdf('outcome_pindep_fit.nc')
+random_pindep_fit = az.from_netcdf('random_pindep_fit.nc')
+reverse_pindep_fit = az.from_netcdf('reverse_pindep_fit.nc')
+standard_pindep_fit = az.from_netcdf('standard_pindep_fit.nc')
+uncued_pindep_fit = az.from_netcdf('uncued_pindep_fit.nc')
+
+loss_pindepstar_fit = az.from_netcdf('loss_pindep-star_fit.nc')
+outcome_pindepstar_fit = az.from_netcdf('outcome_pindep-star_fit.nc')
+random_pindepstar_fit = az.from_netcdf('random_pindep-star_fit.nc')
+reverse_pindepstar_fit = az.from_netcdf('reverse_pindep-star_fit.nc')
+standard_pindepstar_fit = az.from_netcdf('standard_pindep-star_fit.nc')
+uncued_pindepstar_fit = az.from_netcdf('uncued_pindep-star_fit.nc')
+
+###LOAD IN DATA###
+
+fnames = ['BH09_raw-free_S1-5_corrected.xlsx','CH02_corrected.xlsx','NA01_raw_free-choice_S8-18.xlsx',"CH01_corrected.xlsx"]
+df = md.load_multiple_data(fnames, reset_sessions = True)
+
+###RENAME DATA###
+
+for i in range(len(df)):
+    if df.at[i, 'MSN'] == 'LossrGT_A-losscue_v1':
+        df.at[i,'MSN'] = 'outcomeRGT_A'
+    if df.at[i, 'MSN'] == 'LossrGT_B-losscue_v1':
+        df.at[i,'MSN'] = 'outcomeRGT_B'
+        
+#rename MSNs so that the rats on the random task don't have "loss" in the MSN
+for i in range(len(df)):
+    if df.at[i,'MSN'] == 'AnarchyrGT_B-losscue_v6':
+        df.at[i,'MSN'] = 'RandomRGT_B'
+    if df.at[i,'MSN'] == 'AnarchyrGT_A-losscue_v6':
+        df.at[i,'MSN'] = 'RandomRGT_A'
+        
+task_list = df.groupby(['MSN'])['Subject'].unique()
+
+###DEFINE SUBS###
+
+uncued_subs = np.concatenate(task_list[[task for task in df.MSN.unique() if 'Classic' in task]])
+standard_subs = np.concatenate((task_list['rGT_A-cue'], task_list['rGT_B-cue']))
+reverse_subs = np.concatenate((np.concatenate(task_list[[task for task in df.MSN.unique() if 'Mis' in task]]),
+                              np.concatenate(task_list[[task for task in df.MSN.unique() if 'Rev' in task]])))
+outcome_subs = np.concatenate(task_list[[task for task in df.MSN.unique() if 'outcome' in task]])
+random_subs = np.concatenate(task_list[[task for task in df.MSN.unique() if 'Random' in task]])
+loss_subs = np.concatenate(task_list[[task for task in df.MSN.unique() if 'oss' in task]])
+
+###WAIC LISTS###
+
+loss_basic_waic = wc.waic_fit(df, 5, loss_subs, loss_basic_fit, "basic")
+outcome_basic_waic = wc.waic_fit(df, 5, outcome_subs, outcome_basic_fit, "basic")
+random_basic_waic = wc.waic_fit(df, 5, random_subs, random_basic_fit, "basic")
+reverse_basic_waic = wc.waic_fit(df, 5, reverse_subs, reverse_basic_fit, "basic")
+standard_basic_waic = wc.waic_fit(df, 5, standard_subs, standard_basic_fit, "basic")
+uncued_basic_waic = wc.waic_fit(df, 5, uncued_subs, uncued_basic_fit, "basic")
+
+basic_list = [loss_basic_waic.waic, outcome_basic_waic.waic, random_basic_waic.waic, reverse_basic_waic.waic, standard_basic_waic.waic, uncued_basic_waic.waic]
+basic_list_SE = [loss_basic_waic.waic_se, outcome_basic_waic.waic_se, random_basic_waic.waic_se, reverse_basic_waic.waic_se, standard_basic_waic.waic_se, uncued_basic_waic.waic_se]
+
+###
+
+loss_basicstar_waic = wc.waic_fit(df, 5, loss_subs, loss_basicstar_fit, "basicstar")
+outcome_basicstar_waic = wc.waic_fit(df, 5, outcome_subs, outcome_basicstar_fit, "basicstar")
+random_basicstar_waic = wc.waic_fit(df, 5, random_subs, random_basicstar_fit, "basicstar")
+reverse_basicstar_waic = wc.waic_fit(df, 5, reverse_subs, reverse_basicstar_fit, "basicstar")
+standard_basicstar_waic = wc.waic_fit(df, 5, standard_subs, standard_basicstar_fit, "basicstar")
+uncued_basicstar_waic = wc.waic_fit(df, 5, uncued_subs, uncued_basicstar_fit, "basicstar")
+
+basicstar_list = [loss_basicstar_waic.waic, outcome_basicstar_waic.waic, random_basicstar_waic.waic, reverse_basicstar_waic.waic, standard_basicstar_waic.waic, uncued_basicstar_waic.waic]
+basicstar_list_SE = [loss_basicstar_waic.waic_se, outcome_basicstar_waic.waic_se, random_basicstar_waic.waic_se, reverse_basicstar_waic.waic_se, standard_basicstar_waic.waic_se, uncued_basicstar_waic.waic_se]
+
+###
+
+loss_pscale_waic = wc.waic_fit(df, 5, loss_subs, loss_pscale_fit, "pscale")
+outcome_pscale_waic = wc.waic_fit(df, 5, outcome_subs, outcome_pscale_fit, "pscale")
+random_pscale_waic = wc.waic_fit(df, 5, random_subs, random_pscale_fit, "pscale")
+reverse_pscale_waic = wc.waic_fit(df, 5, reverse_subs, reverse_pscale_fit, "pscale")
+standard_pscale_waic = wc.waic_fit(df, 5, standard_subs, standard_pscale_fit, "pscale")
+uncued_pscale_waic = wc.waic_fit(df, 5, uncued_subs, uncued_pscale_fit, "pscale")
+
+pscale_list = [loss_pscale_waic.waic, outcome_pscale_waic.waic, random_pscale_waic.waic, reverse_pscale_waic.waic, standard_pscale_waic.waic, uncued_pscale_waic.waic]
+pscale_list_SE = [loss_pscale_waic.waic_se, outcome_pscale_waic.waic_se, random_pscale_waic.waic_se, reverse_pscale_waic.waic_se, standard_pscale_waic.waic_se, uncued_pscale_waic.waic_se]
+
+###
+
+loss_pscalestar_waic = wc.waic_fit(df, 5, loss_subs, loss_pscalestar_fit, "pscalestar")
+outcome_pscalestar_waic = wc.waic_fit(df, 5, outcome_subs, outcome_pscalestar_fit, "pscalestar")
+random_pscalestar_waic = wc.waic_fit(df, 5, random_subs, random_pscalestar_fit, "pscalestar")
+reverse_pscalestar_waic = wc.waic_fit(df, 5, reverse_subs, reverse_pscalestar_fit, "pscalestar")
+standard_pscalestar_waic = wc.waic_fit(df, 5, standard_subs, standard_pscalestar_fit, "pscalestar")
+uncued_pscalestar_waic = wc.waic_fit(df, 5, uncued_subs, uncued_pscalestar_fit, "pscalestar")
+
+pscalestar_list = [loss_pscalestar_waic.waic, outcome_pscalestar_waic.waic, random_pscalestar_waic.waic, reverse_pscalestar_waic.waic, standard_pscalestar_waic.waic, uncued_pscalestar_waic.waic]
+pscalestar_list_SE = [loss_pscalestar_waic.waic_se, outcome_pscalestar_waic.waic_se, random_pscalestar_waic.waic_se, reverse_pscalestar_waic.waic_se, standard_pscalestar_waic.waic_se, uncued_pscalestar_waic.waic_se]
+
+###
+
+loss_pindep_waic = wc.waic_fit(df, 5, loss_subs, loss_pindep_fit, "pindep")
+outcome_pindep_waic = wc.waic_fit(df, 5, outcome_subs, outcome_pindep_fit, "pindep")
+random_pindep_waic = wc.waic_fit(df, 5, random_subs, random_pindep_fit, "pindep")
+reverse_pindep_waic = wc.waic_fit(df, 5, reverse_subs, reverse_pindep_fit, "pindep")
+standard_pindep_waic = wc.waic_fit(df, 5, standard_subs, standard_pindep_fit, "pindep")
+uncued_pindep_waic = wc.waic_fit(df, 5, uncued_subs, uncued_pindep_fit, "pindep")
+
+pindep_list = [loss_pindep_waic.waic, outcome_pindep_waic.waic, random_pindep_waic.waic, reverse_pindep_waic.waic, standard_pindep_waic.waic, uncued_pindep_waic.waic]
+pindep_list_SE = [loss_pindep_waic.waic_se, outcome_pindep_waic.waic_se, random_pindep_waic.waic_se, reverse_pindep_waic.waic_se, standard_pindep_waic.waic_se, uncued_pindep_waic.waic_se]
+
+###
+
+loss_pindepstar_waic = wc.waic_fit(df, 5, loss_subs, loss_pindepstar_fit, "pindepstar")
+outcome_pindepstar_waic = wc.waic_fit(df, 5, outcome_subs, outcome_pindepstar_fit, "pindepstar")
+random_pindepstar_waic = wc.waic_fit(df, 5, random_subs, random_pindepstar_fit, "pindepstar")
+reverse_pindepstar_waic = wc.waic_fit(df, 5, reverse_subs, reverse_pindepstar_fit, "pindepstar")
+standard_pindepstar_waic = wc.waic_fit(df, 5, standard_subs, standard_pindepstar_fit, "pindepstar")
+uncued_pindepstar_waic = wc.waic_fit(df, 5, uncued_subs, uncued_pindepstar_fit, "pindepstar")
+
+pindepstar_list = [loss_pindepstar_waic.waic, outcome_pindepstar_waic.waic, random_pindepstar_waic.waic, reverse_pindepstar_waic.waic, standard_pindepstar_waic.waic, uncued_pindepstar_waic.waic]
+pindepstar_list_SE = [loss_pindepstar_waic.waic_se, outcome_pindepstar_waic.waic_se, random_pindepstar_waic.waic_se, reverse_pindepstar_waic.waic_se, standard_pindepstar_waic.waic_se, uncued_pindepstar_waic.waic_se]
+
+###CREATE AND EXPORT DATAFRAME###
+
+waic_values = pd.DataFrame(
+    {'basic_waic': basic_list,
+     'basic_waic_SE': basic_list_SE, 
+     'basicstar_waic': basicstar_list,
+     'basicstar_waic_SE': basicstar_list_SE,
+     'pscale_waic': pscale_list,
+     'pscale_waic_SE': pscale_list_SE,
+     'pscalestar_waic': pscalestar_list,
+     'pscalestar_SE': pscalestar_list_SE,
+     'pindep_waic': pindep_list,
+     'pindep_waic_SE': pindep_list_SE,
+     'pindepstar_waic': pindepstar_list,
+     'pindepstar_waic_SE': pindepstar_list_SE},
+     index = ["loss", "outcome", "random", "reverse", "standard", "uncued"])
+
+waic_values.to_excel("waic_values.xlsx")
